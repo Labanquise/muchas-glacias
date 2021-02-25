13e234e408e5ce2ce819f30287e4f66af2d1d471
Frackher <265578+Frackher@users.noreply.github.com>
2021-02-25 14:37:59 +0100

    Pepper Index score calculated [see console]

--- themes/frozenTacos/assets/js/main.js
+++ themes/frozenTacos/assets/js/main.js
@@ -1,29 +1,62 @@
-
-        
-
-
-
-
-// Launch Pepper Index
-function launch(url)
-{
+// Launch Pepper Index Score
+async function launch(url){
     //Create New StyleSheet
     var styleEl = document.createElement('style');
     styleEl.setAttribute("id", "animationSS");
     document.head.appendChild(styleEl);
-    //var styleSheet = styleEl.sheet;
 
-    //styleSheet.insertRule('header .version { background-color: white }', 0);
     //Verify if the URL is valid
     if(!checkURL(url))
         alert("Veuillez entrer une URL valide")
     else {
-        //Launch the different test
-        googleLH(url, "performance", "perf");
-        googleLH(url, "accessibility", "acc");
-        googleLH(url, "best-practices", "bp");
-        googleLH(url, "seo", "seo");
+        let data = await getScores(url);
+        getPepper(data);
     }
+}
+
+
+// Launch Pepper Index
+async function getScores(url){
+    //Launch the different test
+    console.log("calling");
+    const responses = await Promise.all([
+        ecoindex(url),
+        googleLH(url, "performance", "perf"),
+        googleLH(url, "accessibility", "acc"),
+        googleLH(url, "best-practices", "bp"),
+        googleLH(url, "seo", "seo")
+    ]);
+    console.log(responses)
+    return responses;
+    console.log("end of call");
+
+}
+
+//Calculate Pepper Index
+function getPepper(data){
+    /*
+    repartition peperdindex  : 10 / 15 / 20 / 25 / 30
+    formule corrigée : (40A +20B + 20 C + 10 D + 10E ) / 100
+    */
+   let score = (40*data[0]+20*data[1]+20*data[2]+10*data[3]+10*data[4])/100;
+   let grade = getPepperGrade(score);
+   console.log("Pepper Index :"+score+" et "+grade);
+}
+
+//Getting Grade depending on Score
+function getPepperGrade(score){
+    if(score > 100 || score < 0) return "INVALID SCORE";
+    var map = [
+        {max: 85, grade: "A"},
+        {max: 70, grade: "B"},
+        {max: 50, grade: "C"},
+        {max: 30, grade: "D"},
+    ];
+    for(var loop = 0; loop < map.length; loop++) {
+        var data = map[loop];
+        if(score >= data.max) return data.grade;
+    }
+    return "F";
 }
 
 // Validating URL
@@ -38,18 +71,33 @@ function checkURL(url)
       return true;
 }
 
+async function ecoindex(url2Test){
+    return await fetch('https://ecoindex.muchas-glacias.com', {
+        method: 'POST',
+        body: '{"URL":"'+url2Test+'"}'
+    })
+    .then(response => response.json())
+    .then(json => {
+        testTube("ei", parseInt(json.Score));
+        //specific return in the then for the promise.all
+        return parseInt(json.Score);
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
 // Asking LightHouse
-function googleLH(url, type, id)
+async function googleLH(url, type, id)
 {
-    console.log(url);
     const req = setUpQuery(url, type);
-    fetch(req)
+    return await fetch(req)
         .then(response => response.json())
         .then(json => {
             const lhRes = json.lighthouseResult;
-            console.log("réponse : "+lhRes.categories[type].score);
-            testTube(id, lhRes.categories[type].score*100);
-            //document.getElementById(id).innerHTML = lhRes.categories[type].score*100;
+            testTube(id, parseInt(lhRes.categories[type].score*100));
+            //specific return in the then for the promise.all
+            return parseInt(lhRes.categories[type].score*100);
         });
 }
 
@@ -59,7 +107,7 @@ function setUpQuery(url, type) {
     const parameters = {
       url: encodeURIComponent(url),
       category: type,
-      strategy: "desktop",
+      strategy: "mobile",
       key: "AIzaSyBDMfNxNncSxZkXrWob_o7pyqBRFGYt0GI",
     };
     let query = `${api}?`;
@@ -76,19 +124,32 @@ function testTube(id, score)
         console.log("error null");
     else{
         color = "";
-        if(score >= 75) {color = "green"} else
-        if(score >= 25 && score < 75) {color = "orange"} else
-        if(score >= 0 && score < 25) {color = "red"}
+        colorHex = "";
+        if(score >= 75) {color = "green";colorHex = "#1CAF9C";} else
+        if(score >= 25 && score < 75) {color = "orange";colorHex = "#F4AB00";} else
+        if(score >= 0 && score < 25) {color = "red";colorHex = "#C8222C";}
 
-       
-
-        
-    
-        document.getElementById("res-"+id).classList.add(color);
-        document.getElementById("s-"+id).innerHTML = score;
-
+        //Define specific SS rules
         var ss = document.getElementById("animationSS").sheet;
-        ss.insertRule('.mg .resultats #res-'+id+'.pill { background-color: white }', ss.cssRules.length);
-        console.log("d "+ss.cssRules.length);
+        var extendSlime = ((score*9)/100)+4.5;
+        var extendLevel = ((score*9)/100)+3.75;
+
+        var rules = [
+            '@keyframes slime-'+id.toUpperCase()+' {from {height: 4.5rem;} to {height: '+extendSlime+'rem;}}',
+            '@keyframes level-up-'+id.toUpperCase()+' {from {bottom:3.75rem;} to {bottom: '+extendLevel+'rem}}',
+            '@keyframes mergeColor-'+id.toUpperCase()+' {from {color: $c_white;} to {color: '+colorHex+';}}'
+        ];
+
+        rules.forEach(item => {
+            ss.insertRule(item, ss.cssRules.length);
+        });
+        
+
+        //Add Score + Color + Animated
+        if(score<41)
+            document.getElementById("res-"+id).classList.add(color,"mixDiff","animated");
+        else
+        document.getElementById("res-"+id).classList.add(color,"animated");
+        document.getElementById("s-"+id).innerHTML = score;
     }
 }
